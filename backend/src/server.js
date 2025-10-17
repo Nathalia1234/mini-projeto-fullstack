afa06c6cd54bd37540ba2fe0803ff7795cb23ff7
Nathalia1234 <nathe557@gmail.com>
2025-10-17 20:01:10 -0300

    -

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API do Mini-Projeto Fullstack está online 🚀" });
+});
